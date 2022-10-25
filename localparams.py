84b10d50ada6c1f0d747f198002dea0a178048cc
perceptronv <neutrinovs@gmail.com>
2022-10-25 10:00:04 +0200

    Localparams update

--- localparams.py
+++ localparams.py
@@ -1,5 +1,5 @@
 LOCALPARAMS = {
     'PORT': '/dev/ttyACM0',
-    'DATADIR': '/media/pi/ADATA UFD/TECLARS/data',
+    'LOADDIR': '/media/pi/ADATA UFD/TECLARS/data',
     'OUTDIR': '/media/pi/ADATA UFD/TECLARS-reports/'
 }
